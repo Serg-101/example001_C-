4f09e10f30408641b4a3a3cd643a05751dfdeb5e
Serg-101 <golikov1979@gmail.com>
2022-04-25 20:56:59 +0300

    Add prj

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число -> ");
+int i = int.Parse(Console.ReadLine());
+//int i = Convert.ToInt32(Console.ReadLine());
+i = i * i;
+Console.WriteLine("Квадрат числа -> " + i);
+Console.Write("Квадрат числа -> ");
+Console.WriteLine(i);
+Console.ReadKey();
